f946e7604ef6b329da7533b977463ff658a46399
welington2632 <xdwelington@hotmail.com>
2023-03-03 18:10:24 -0300

    correcoes mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,27 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer id="contato" className={styles.footer}>
-      <p>Entre em contato</p>
-      <p>
-        <a href="mailto:hege@example.com">xdwelington@hotmail.com.br</a>
-      </p>
-      <hr />
-      <p>ou via whatsapp</p>
-      <p>
-        <a>(11) 95493-0363</a>
-      </p>
+      <div className={styles.footer_menu_container}>
+        <span className={styles.footer_menu_title}>Contatos</span>
+        <ul>
+          <li>
+            <a href="mailto:hege@example.com">xdwelington@hotmail.com.br</a>
+          </li>
+          <li>
+            <a href="">(11) 95493-0363</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.footer_menu_container}>
+        <span className={styles.footer_menu_title}>Redes sociais</span>
+        <ul>
+          <li>
+            <a target="_blank" href="https://www.linkedin.com/in/welington-martins/">
+              Linkedin
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }

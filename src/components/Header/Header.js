6fb2d52497a9212ae74da9a4c8325537c90a9140
welington2632 <xdwelington@hotmail.com>
2023-03-15 14:10:26 -0300

    ajuste responsivo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,23 +9,6 @@ export default function Header({ logo_src }) {
         <Link href="/" className={styles.logo}>
           Welington Martins
         </Link>
-        <ul className={styles.header_menu}>
-          <li>
-            <Link href="/" className={styles.header_menu_item}>
-              Inicio
-            </Link>
-          </li>
-          <li>
-            <Link href="/sobre" className={styles.header_menu_item}>
-              Sobre
-            </Link>
-          </li>
-          <li>
-            <Link href="/contato" className={styles.header_menu_item}>
-              Contato
-            </Link>
-          </li>
-        </ul>
       </div>
     </header>
   );
